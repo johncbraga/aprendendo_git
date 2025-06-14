2ee1cbae27aacad5f5c0c8acc801dccb03237dfa
John Braga <johngitrepository@gmail.com>
2025-06-14 08:29:04 -0300

    Mudança de dificuldade

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 alert('Hello World!');
 
 // Definição da variável onde o valor máximo utilizado no jogo será atribuido
-let valorMaximo = 10;
+let valorMaximo = 15;
 
 // Cria a variável do número secreto e gera um número aleatório
 let numeroSecreto = parseInt(Math.random() * valorMaximo + 1);
